f255620ced4bc25a59af836f1d214bcffbaf899e
Railan Santana <lansilvah14fsa@gmail.com>
2024-06-27 12:08:23 -0300

    feat: implementation refereces from authordto

--- src/main/java/com/railansantana/workshop/config/Instantiation.java
+++ src/main/java/com/railansantana/workshop/config/Instantiation.java
@@ -40,6 +40,8 @@ public class Instantiation implements CommandLineRunner {
 		Post p2 = new Post(null, sdf.parse("21/03/2018"), "Titulo do post 2", "corpo post 2", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(p1, p2));
 
+		maria.getPosts().addAll(Arrays.asList(p1, p2));
+		userRepository.save(maria);
 	}
 
 }
