5999b1cdb243a931fe2c9d9ed3d8c1480c00cf2a
Railan Santana <lansilvah14fsa@gmail.com>
2024-06-27 22:42:43 -0300

    project finalized

--- src/main/java/com/railansantana/workshop/repository/PostRepository.java
+++ src/main/java/com/railansantana/workshop/repository/PostRepository.java
@@ -18,6 +18,5 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	@Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
 	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
-
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
